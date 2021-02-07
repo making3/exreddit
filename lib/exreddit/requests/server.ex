a5ea84b708720a038b71de50087926a05038e507
Zarathustra2 <dario.heinisch@gmail.com>
2021-02-07 02:04:12 +0100

    Infinity call

--- lib/exreddit/requests/server.ex
+++ lib/exreddit/requests/server.ex
@@ -13,7 +13,7 @@ defmodule ExReddit.Requests.Server do
       Request.get_with_token(uri, token, opts)
     end
 
-    GenServer.call(__MODULE__, {:request, req})
+    GenServer.call(__MODULE__, {:request, req}, :infinity)
   end
 
   ## Server API
